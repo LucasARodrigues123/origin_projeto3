9dd6610c44a1aeb19ae4647aff71eb325aaef8f6
LucasARodrigues123 <esscur1dao@gmail.com>
2023-10-02 21:29:51 -0300

    contruindo menu inicial

---
+++ cadastro.py
@@ -0,0 +1,48 @@
+import re
+import json
+import match
+
+# criar banco de dados
+
+# criar lista de pessoas
+
+# criar menu 
+class Menu_inicial:
+    def __init__(self, nome, idade, cidade, email, cpf): 
+        self.nome = nome
+        self.idade = idade
+        self.cidade = cidade
+        self.email = email
+        self.cpf = cpf
+        pass
+
+    def menu():
+        match menu:
+        case 1:
+            print("Listar Pessoas tecle:1")
+        case 2:
+            print("Cadastrar uma Pessoas tecle:2")
+        case 3:
+            print("Editar um Pessoa tecle:3")
+        case 4:
+            print("Procurar uma Pessoa tecle:4")
+        case 5:
+            print("Deletar uma Pessoa tecle:5")
+        case 6:
+            print("Sair do Programa tecle:6")
+        case _:
+            print("Escolha uma das Opesoes Acima")
+
+ pessoa = [] 
+
+
+
+
+# criar cadastrar pessoas
+
+# listar pessoas 
+
+# modificar pessoas
+
+# deletar pessoas
+
