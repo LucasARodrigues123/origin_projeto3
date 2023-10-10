9b128d827eaaa603d571368a128b846b26a10d0d
LucasARodrigues123 <esscur1dao@gmail.com>
2023-10-09 21:02:38 -0300

    adicionado funsão

--- cadastro.py
+++ cadastro.py
@@ -1,44 +1,42 @@
 import json
 
+try:
+    with open("dados.json", 'r',) as arquivo:
+        dados_lidos = json.load(arquivo)
+        numero_de_pessoas = len(dados_lidos)
+        print(f"Número de objetos no banco de dados: {numero_de_pessoas}")
+except FileNotFoundError:
+    print("O arquivo 'banco_de_dados.json' não foi encontrado.")
 
-# Definir o banco de dados JSON
-banco_de_dados = {
-    "Pessoa": [
-        {
-            "nome": "Lucas",
-            "idade": 24,
-            "peso": 58,
-            "altura": 1.70,
-            "nascimento": "30/05/1999",
-            "cpf": "135.140.21",
-            "email": "esscur11dao@gmail.com"
-        }
-    ]
-}
+def minha_funsao():
+    with open('dados.jon',"r") as jon:
+        funsao = json.load(jon)
+        return funsao
+    
+def procurar_pessoa():
+    for busca in minha_funsao:
+        
 
-
-# Salvar o banco de dados JSON em um arquivo
-with open("banco_de_dados.json", "w") as arquivo_json:
-    json.dump(banco_de_dados, arquivo_json)
 
 # Criar um menu simples
 def menu():
     print("Bem-vindo ao Menu de Cadastro")
-    print("1. Listar Pessoas")
+    print("1. Procurar uma Pessoa")
     print("2. Cadastrar uma Pessoa")
     print("3. Editar uma Pessoa")
-    print("4. Procurar uma Pessoa")
-    print("5. Deletar uma Pessoa")
-    print("6. Sair do Programa")
+    print("4. Deletar uma Pessoa")
+    print("5. Sair do Programa")
 
+ 
 # Loop principal do programa
 while True:
     menu()
     escolha = input("Escolha uma opção: ")
     
     if escolha == "1":
-        # Lógica para listar pessoas
+        # Lógica para procurar pessoa
         pass
+
     elif escolha == "2":
         # Lógica para cadastrar uma pessoa
         pass
@@ -46,8 +44,11 @@ while True:
         # Lógica para editar uma pessoa
         pass
     elif escolha == "4":
-        # Lógica para procurar uma pessoa
         pass
+        # Lógica para procurar uma pessoa
+        # # cadastro = sorted(, key=lambda pessoa: pessoa['Nome'])
+        # for pessoa in cadastro:
+        #     print(f'Nome:{pessoa["Nome"]},')
     elif escolha == "5":
         # Lógica para deletar uma pessoa
         pass
